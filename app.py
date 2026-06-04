9eb878fa9b0421752f4085bbea0f5bce8f30a5b2
Harshitha <harshithaganta0707@gmail.com>
2026-06-04 10:16:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+
+    file = request.files['image']
+
+    npimg = np.frombuffer(file.read(), np.uint8)
+
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    brightness = np.mean(gray)
+
+    if brightness > 120:
+        status = "Fresh"
+        score = 85
+    else:
+        status = "Expired"
+        score = 35
+
+    return jsonify({
+        "status": status,
+        "score": score
+    })
+
+if __name__ == "__main__":
+    app.run()
