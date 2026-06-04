0853fa76da8c74ab94dc18331646957c1d68c69c
Harshitha <harshithaganta0707@gmail.com>
2026-06-04 10:35:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,30 +8,9 @@ app = Flask(__name__)
 def home():
     return "WeHelp AI Server Running Successfully"
 
-@app.route('/analyze', methods=['POST'])
-def analyze():
-
-    file = request.files['image']
-
-    npimg = np.frombuffer(file.read(), np.uint8)
-
-    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    brightness = np.mean(gray)
-
-    if brightness > 120:
-        status = "Fresh"
-        score = 85
-    else:
-        status = "Expired"
-        score = 35
-
-    return jsonify({
-        "status": status,
-        "score": score
-    })
+@app.route("/test")
+def test():
+    return "AI Test Successful"
 
 if __name__ == "__main__":
     app.run()
