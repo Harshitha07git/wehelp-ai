6cdb7920e857e3b440baefaa8702aa7c0f921768
Harshitha <harshithaganta0707@gmail.com>
2026-06-04 10:40:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,30 @@ def home():
 def test():
     return "AI Test Successful"
 
+@app.route("/analyze", methods=["POST"])
+def analyze():
+
+    file = request.files["image"]
+
+    npimg = np.frombuffer(file.read(), np.uint8)
+
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    brightness = np.mean(gray)
+
+    if brightness > 120:
+        status = "Fresh"
+        score = 85
+    else:
+        status = "Expired"
+        score = 35
+
+    return jsonify({
+        "status": status,
+        "score": score
+    })
+
 if __name__ == "__main__":
     app.run()
